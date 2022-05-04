e2de6d138fc60b3ecf8417cbf36f4d326412eb06
Kai <83906674+scriptoveride@users.noreply.github.com>
2022-05-04 14:05:14 +1000

    Update Simple Neural Network.py

--- Simple Neural Network in python/Simple Neural Network.py
+++ Simple Neural Network in python/Simple Neural Network.py
@@ -1,78 +1,84 @@
+#the neural network is trying to predict what the value of y is based on x
+
 import numpy as np
 
+def nonlin(x,deriv=False):                    # sigmoid function
 
-# sigmoid function
-def nonlin(x,deriv=False):
     if(deriv==True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
- 
 
 
-# new input dataset
-Z = np.array([[0,1,1],
+Z = np.array([[0,0,1],                          # new input dataset
+              [0,1,1],
               [1,1,1],
+              [1,1,0],
               [1,1,1],
-              [1,0,1]])
+              [1,1,0],
+              [1,1,0],
+              [1,0,0],
+              [1,0,0]])
 
 
-# input dataset
+                                                # input dataset
 X = np.array([[0,0,1],
-              [1,0,1],
+              [0,1,1],
               [1,1,1],
-              [0,1,1]])
+              [1,0,1],
+              [1,1,0],
+              [1,0,0]])
     
+          
+y = np.array([[1,1,1,1,0,0]]).T                                # output dataset  
 
-# output dataset            
-y = np.array([[0,
-               1,
-               1,
-               0]]).T
+np.random.seed(1)                               # seed random numbers to make calculation
 
-
-# seed random numbers to make calculation
-# deterministic (just a good practice)
-np.random.seed(1)
-
-# initialize weights randomly with mean 0
-syn0 = 2*np.random.random((3,1)) - 1
+syn0 = 2*np.random.random((3,1)) - 1            # initialize weights randomly with mean 0
 
 for iter in range(10000):
 
-    # forward propagation
-    l0 = X
+   
+    l0 = X                                       # forward propagation
     l1 = nonlin(np.dot(l0,syn0))
-
-    # how much did we miss?
-    l1_error = y - l1
-
-    # multiply how much we missed by the 
-    # slope of the sigmoid at the values in l1
-    l1_delta = l1_error * nonlin(l1,True)
-
-    # update weights
-    syn0 += np.dot(l0.T,l1_delta)
     
-    #print(l1, '\n')
-
-
-
+    l1_error = y - l1                           # how much did it miss 
+    
+    l1_delta = l1_error * nonlin(l1,True)       # multiply how much we missed by the slope of the sigmoid at the values in l1
+    
+    syn0 += np.dot(l0.T,l1_delta)               # update weights
 
 
 def new_data():
-    # forward propagation
-    l0 = Z
+    
+    l0 = Z                                       # forward propagation
     l1 = nonlin(np.dot(l0,syn0))
     print(l1, '\n')
     
+    
+    number = 0 
+    for i in range(len(l1)):
+        if l1[number] > 0.5:
+            print("1")
+        else:
+            print("0")
+        number += 1
 
-
-  
+        
 print("\nStarting Data\n", X, "\n\n", y, "\n")
 print("Output After Training: ")
 print(l1)
+print("\n")
 
 
-print("\n\n\nStarting Data")  
+number = 0 
+for i in range(len(l1)):
+    if l1[number] > 0.5:
+        print("1")
+    else:
+        print("0")
+    number += 1
+    
+
+print("\n\nStarting Data")  
 print(Z, "\n")
 new_data()
